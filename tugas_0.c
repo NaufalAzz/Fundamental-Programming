28d2c19cae4a0b549e2812bed42d926893071892
Ahmad Naufal Azzuhdi <azzuhdinaufalahmad@gmail.com>
2024-10-14 20:39:40 +0700

    Add files via upload

---
+++ tugas_0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+
+{
+    int a, b, sum;
+    printf("enter first integer\n");
+    scanf("%d", &a);
+    printf("enter second integer\n");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("sum = %d\n", sum);
+    return 0;
+}
