f2aac3b9a955fdef1cd2de71ebde2dcbed9dd440
Ahmad Naufal Azzuhdi <azzuhdinaufalahmad@gmail.com>
2024-10-16 09:41:07 +0700

    Add files via upload

---
+++ modul1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+int main()
+
+{
+    //ini adalah komentar satu baris
+    printf("hello world \n");
+    
+    /*
+    ini adalah komentar yg lebih serius dari satu baris
+    */
+    return 0;
+}
